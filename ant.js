b49cbb701af69a865d1614904bc9a084295f9331
nam <namjjd@gmail.com>
2016-01-09 14:41:43 +0800

    working on paths

--- ant.js
+++ ant.js
@@ -1,9 +1,13 @@
-function Ant (x, y) {
+function Ant (x, y, r) {
     this.x = x;
     this.y = y;
-    this.maxvx = 5;
-    this.maxvy = 5;
+    this.radius = r;
+    this.maxvx = 1;
+    this.maxvy = 1;
     this.pheromonerate = 0.1
+    this.foodcapacity = 0.5;
+    this.food = 0;
+    this.checkpoint = undefined;
 }
 
 Ant.prototype.reducePheromone = function () {
@@ -11,12 +15,50 @@ Ant.prototype.reducePheromone = function () {
 }
 
 Ant.prototype.decideMove = function (allAnts) {
-    //Calculate in radius
     
-    //temp
-    this.x += Math.random() * 1 - 0.5;
-    this.y += Math.random() * 1 - 0.5;
+
+    // In exploration mode, ant will set a checkpoint within a certain radius 
+    // And make its way there in the general direction
+    
+    if (this.checkpoint === undefined) {
+        //generate new checkpoint
+        
+        var rad = (Math.random() * 2 * Math.PI)
+        var cpdist = 120 + (Math.random() * 70 - 35);
+
+        var newx = this.x + cpdist * Math.cos(rad);
+        var newy = this.y + cpdist * Math.sin(rad);
+        this.checkpoint = [newx, newy];
+
+    } else if (collideptant(this, this.checkpoint, 5)){
+        this.checkpoint = undefined; 
+    }else {
+        //move in direction of checkpoint
+        if (this.checkpoint[0] > this.x) {
+            this.x += Math.random() * this.maxvx;
+        } else {
+            this.x -= Math.random() * this.maxvx;
+        }
+
+        if (this.checkpoint[1] > this.y) {
+            this.y += Math.random() * this.maxvy;
+        } else {
+            this.y -= Math.random() * this.maxvy;
+        }
+    }
 }
 Ant.prototype.getPos = function () {
     return [this.x,this.y];
+}
+//Check if object is within certain radius of point
+function collideptant(ant, tgt, dist) {
+    var tgtx = tgt[0];
+    var tgty = tgt[1];
+    
+    var dx = Math.abs(ant.x - tgtx);
+    var dy = Math.abs(ant.y - tgty);
+    
+    var pyth = Math.sqrt(dx*dx + dy*dy);
+
+    return pyth < dist;
 }
--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 var canvas;
 var ctx;
 
-var rows = 200;
-var cols = 200;
+var rows = 20;
+var cols = 20;
 
 var ants = [];
 var fabricAnts = [];
 var fabricFood = [];
-function redraw() {
+var fabricPheromones = [];
+var pathPoints = [];
+
+function reDraw() {
     //If position change, redraw
 
     for(var i = 0; i < fabricAnts.length; i++ ){
@@ -21,22 +24,50 @@ function redraw() {
     canvas.renderAll();
 }
 
-function recalc() {
+function reCalcAnts() {
     for(var i = 0; i < ants.length; i++ ) {
         ants[i].decideMove();
     }
 }
+function addPheromones() {
+    
+}
 
+function setUpPheromones() {
+    var width = 300 / cols;
+    var height = 300 / rows;
+    for (var i = 0; i < rows; i++) {
+        fabricPheromones[i] = new Array();
+        for (var j = 0; j < cols; j++) {
+            fabricPheromones[i].push(new fabric.Rect({
+                fill: "#7EC0EE",
+                width: width,
+                height: height,
+                top: Math.floor(i*height),
+                left: Math.floor(j*width),
+                selectable:false
+            }));
+            console.log(i+" "+j);
+            canvas.add(fabricPheromones[i][j]);
+        }
+    }
+    canvas.renderAll();
+}
 function refresh(x) {
     console.log(x); 
-    recalc();
-    redraw();
-    if (x < 2000) {
+    reCalcAnts();
+    addPheromones();
+    reDraw();
+    if (x < 20000) {
         window.requestAnimationFrame(refresh);
     }
 }
 $(document).ready(function () {
     canvas = new fabric.Canvas('c');
+    
+    canvas.setHeight(300);
+    canvas.setWidth(300);
+
     ctx = canvas.getContext('2d');
     ctx.fillStyle="pink";
     ctx.fillRect(100, 100, 20, 20);
@@ -51,16 +82,19 @@ $(document).ready(function () {
          
         fabricFood.push(new fabric.Circle({
             radius: 3,
+            originX: 'center',
+            originY: 'center',
             fill: 'black',
             left: mouseX - 3,
             top: mouseY - 3,
         }));
         canvas.add(fabricFood[fabricFood.length - 1]);
     });
-
+    
+    setUpPheromones(); 
 
     for(var i = 0; i < 9; i++) {
-        ants.push(new Ant(spawnX, spawnY)); 
+        ants.push(new Ant(spawnX, spawnY, 2)); 
         fabricAnts.push(new fabric.Circle({
             radius: 2,
             fill: 'maroon',
@@ -69,6 +103,5 @@ $(document).ready(function () {
         }));
         canvas.add(fabricAnts[i]);
     }
-
     window.requestAnimationFrame(refresh);
 });
